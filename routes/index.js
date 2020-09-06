6b4fae895bfb77e0291566472e2f8b87fbb58fde
cjkook <cjko.audio@gmail.com>
2020-09-06 17:45:52 -0600

    deploy issues

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,8 @@ const apiRoutes = require("./api");
 router.use("/api", apiRoutes);
 
 // If no API routes are hit, send the React app
-router.use("*", function(req, res, next) {
-  console.log(req.originalUrl);
+router.use(function(req, res, next) {
+  if (/\/.+\..+$/i.test(req.originalUrl)) return next();
   if (process.env.NODE_ENV === 'production' && /get/i.test(req.method) && req.accepts('html')) {
     res.sendFile(path.join(__dirname, "../client/build/index.html"));
   }
