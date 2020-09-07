4ce672adb0654d2f5ba599addfe3c68ff2faf304
cjkook <cjko.audio@gmail.com>
2020-09-07 14:33:50 -0600

    routing

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,22 @@ const apiRoutes = require("./api");
 router.use("/api", apiRoutes);
 
 // If no API routes are hit, send the React app
-router.get(function(req, res, next) {
-  if (process.env.NODE_ENV === 'production') {
-    if (/\/([^\/])+\.([^\/])+$/) return next();
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+router.use(function(req, res, next) {
+  console.log(req.originalUrl, req.accepts('html'));
+  if (process.env.NODE_ENV === 'production' && /get/i.test(req.method) && req.accepts('html')) {
+    try {
+      return res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+    } catch (err) {
+      console.log('404 failed to serve index.html -', err);
+    }
   }
-  next();
+  if (req.accepts('json')) {
+    return res.json({
+      status: 404,
+      statusText: 'Not Found',
+    });
+  }
+  res.type('txt').send('Not found');
 });
 
 module.exports = router;
