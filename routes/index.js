011e0c6f915e5886b9a81e48bd31d29ef8f8e694
cjkook <cjko.audio@gmail.com>
2020-09-07 14:24:51 -0600

    routing

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,9 @@ router.use("/api", apiRoutes);
 
 // If no API routes are hit, send the React app
 router.get(function(req, res, next) {
-  if (process.env.NODE_ENV === 'production' && /get/i.test(req.method) && req.accepts('html')) {
+
+  if (process.env.NODE_ENV === 'production') {
+    if (/\/([^\/])+\.([^\/])+$/) return next();
     res.sendFile(path.join(__dirname, "../client/build/index.html"));
   }
 });
